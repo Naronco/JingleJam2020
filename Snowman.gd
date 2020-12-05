e3033c37d6094e278dc542440a6996596819b0cb
Johannes Roth <johannes.roth18199@gmail.com>
2020-12-05 20:44:05 +0100

    add example snowman

--- Snowman.gd
+++ Snowman.gd
@@ -3,6 +3,7 @@ extends KinematicBody
 export(float) var throwFrequency = 5.0
 
 var lastThrowTime = 0.0
+var throwCount = 0
 var player
 
 const Snowball = preload("res://Snowball.tscn")
@@ -13,7 +14,7 @@ func _ready():
 func _process(delta):
 	lastThrowTime += delta
 	
-	if lastThrowTime >= 1 / throwFrequency:
+	if lastThrowTime >= 1 / throwFrequency and player != null:
 		# Get player
 		var throwDir = player.transform.origin - transform.origin
 		throwDir.y = 0
@@ -21,9 +22,15 @@ func _process(delta):
 		
 		var snowball = Snowball.instance()
 		snowball.velocity = 8 * throwDir
-		snowball.transform.origin = transform.origin + throwDir
+		
+		var sideOffs = Vector3(throwDir.z, 0, -throwDir.x) * .3
+		if throwCount % 2 == 1:
+			sideOffs = -sideOffs
+		
+		snowball.transform.origin = transform.origin + 0.9 * throwDir + sideOffs + Vector3(0, 0.2, 0)
 		get_parent().add_child(snowball)
 		
+		throwCount += 1
 		lastThrowTime = 0.0
 
 func set_player(p):
