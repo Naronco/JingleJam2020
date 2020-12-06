6109f13dcf277b7e7fbceb2bb7c897fbed790966
Johannes Roth <johannes.roth18199@gmail.com>
2020-12-06 14:47:36 +0100

    gingerbread man walking

--- GingerbreadMan.gd
+++ GingerbreadMan.gd
@@ -1,6 +1,7 @@
 extends KinematicBody
 
-export(float) var throwFrequency = 5.0
+export(float) var jumpFreq = 5.0
+export(float) var jumpVel = 5.0
 
 var lastThrowTime = 0.0
 var throwCount = 0
@@ -22,19 +23,29 @@ func _physics_process(delta):
 	lastThrowTime += delta
 	lastWalkDirChangeTime += delta
 	
+	var gravityDir = PhysicsServer.area_get_param(get_world().get_space(), PhysicsServer.AREA_PARAM_GRAVITY_VECTOR)
+	var gravityStrength = PhysicsServer.area_get_param(get_world().get_space(), PhysicsServer.AREA_PARAM_GRAVITY)
+	
+	var up = -gravityDir.normalized()
+	var velUp = velocity.dot(up) * up
+	var velPlane = velocity - velUp
+	
 	if lastWalkDirChangeTime >= newDirTime:
 		var angle = rng.randf_range(0, 2 * PI)
-		randomWalkDir = Vector3(cos(angle), 0, sin(angle)) * 10.0
+		randomWalkDir = Vector3(cos(angle), 0, sin(angle)) * rng.randf_range(0.5, 2)
 		lastWalkDirChangeTime = 0.0
 		newDirTime = rng.randf_range(0.5, 2.0)
-	
-	velocity = randomWalkDir * delta
-	velocity = move_and_slide(velocity, Vector3(0, 1, 0))
-	
-	if lastThrowTime >= 1 / throwFrequency and player != null:
-		# Get player
-		# Jump to the player
-		pass
+		
+		if rng.randi_range(0, 2) == 0:
+			# jump
+			velUp = up * jumpVel
+
+	# Assume randomWalkDir is parallel to gravity
+	velUp += gravityDir * gravityStrength * delta
+	velPlane = randomWalkDir
+	velocity = velUp + velPlane
+	velocity = move_and_slide(velocity, up)
+
 
 func set_player(p):
 	player = p
