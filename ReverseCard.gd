b1cc0c9df1c4163085b34b0b94af52b92e86949d
WebFreak001 <gh@webfreak.org>
2020-12-06 18:33:22 +0100

    respawn reverse cards

--- ReverseCard.gd
+++ ReverseCard.gd
@@ -1,17 +1,27 @@
 extends Area
 
 var elapsedTime = 0.0
+var respawnTime = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("body_entered", self, "_on_body_enter")
 
 func _on_body_enter(body):
+	if !visible:
+		return
+
 	print("Entered")
 	if body.get_name() == "Player":
 		body.flip_gravity()
-		queue_free()
+		visible = false
+		respawnTime = 10
 
 func _process(delta):
-	elapsedTime += delta
-	$Sprite3D.transform.origin.y = 0.1 + 0.1 * sin(4 * elapsedTime)
+	if !visible:
+		respawnTime -= delta
+		if respawnTime < 0:
+			visible = true
+	else:
+		elapsedTime += delta
+		$Sprite3D.transform.origin.y = 0.1 + 0.1 * sin(4 * elapsedTime)
