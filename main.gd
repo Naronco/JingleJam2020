74b5a143e57e79d81501b5987e7c0b14e9d7e276
TheJmJ <ecthejmj@gmail.com>
2020-12-06 21:41:31 +0200

    Added collision to ControlPanel, added IntroAudio to scene

--- main.gd
+++ main.gd
@@ -6,15 +6,25 @@ extends Spatial
 # var b = "text"
 
 var time = 0
+var audio_file_path = "res://TitleAudio/title"
+var a = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	a.randomize()
+	var val = a.randi_range(1, 9)       # pick a valid random number
+	var path = audio_file_path+str(val)+".wav"
+	if File.new().file_exists(path):
+		var sfx = load(path)
+		$Intro.stream = sfx
+		$Intro.volume_db = -6
+		$Intro.play()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	time += delta
+#func _process(delta):
+	#time += delta
 	#$ColorGradingFilter.temperature = sin(time)
 	#$ColorGradingFilter.update()
 
